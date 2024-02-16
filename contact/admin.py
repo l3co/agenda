182f5bbdbc694723b217800ed597c54d3b7b29f0
leco <l3co@outlook.com>
2024-02-16 19:53:16 -0300

    feat(contact): configure ContactAdmin to filter, list and ordering

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,10 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone_number')
+    ordering = ('-id',)
+    search_fields = ('first_name', 'last_name', 'id')
+    list_per_page = 1
+    list_max_show_all = 100
+    list_editable = ['first_name', 'last_name']
+    list_filter = ['id', 'created_date']
