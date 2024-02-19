7611ff913e0a0edf8c2ccfb0efb702518d127f29
leco <l3co@outlook.com>
2024-02-19 13:44:59 -0300

    refactor(admin):  increase list per page limit

--- contact/admin.py
+++ contact/admin.py
@@ -8,7 +8,7 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'phone_number')
     ordering = ('-id',)
     search_fields = ('first_name', 'last_name', 'id')
-    list_per_page = 1
+    list_per_page = 10
     list_max_show_all = 100
     list_editable = ['first_name', 'last_name']
     list_filter = ['id', 'created_date']
