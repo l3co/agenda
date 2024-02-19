dcbd9354f1addc3a290848c4255a6ef843620434
leco <l3co@outlook.com>
2024-02-19 13:46:26 -0300

    fix(media): configure media url and static root url

--- contact/urls.py
+++ contact/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from contact import views
@@ -7,3 +9,6 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
