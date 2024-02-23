40ff3be7bf9657166740b3de90446895acbe5194
leco <l3co@outlook.com>
2024-02-23 13:18:59 -0300

    style(contact): show my contact list inside index page

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from contact.models import Contact
+
 
 def index(request):
-    return render(request, 'contact/index.html')
+    context = {
+        'contacts': Contact.objects.all()
+    }
+
+    return render(
+        request=request,
+        template_name='contact/index.html',
+        context=context
+    )
