8df23b6dd445fa6062c7b5aefcd09502cfee2e29
leco <l3co@outlook.com>
2024-02-23 09:50:30 -0300

    feat(contact): script to load 1000 randon contacts

--- contact/admin.py
+++ contact/admin.py
@@ -17,4 +17,4 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_max_show_all = 100
     list_editable = ['first_name', 'last_name']
-    list_filter = ['id', 'created_date']
+    list_filter = ['created_date']
--- project/settings.py
+++ project/settings.py
@@ -123,6 +123,11 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
